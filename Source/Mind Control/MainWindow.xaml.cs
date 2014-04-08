a45d838cab57763725975c7176cb6d822431620b
Eric Carlson <mcarlson@student.neumont.edu>
2014-04-08 16:24:42 -0700

    Added connection to the Emotiv Headset. The program does not yet give feedback on connection.

--- Source/Mind Control/MainWindow.xaml.cs
+++ Source/Mind Control/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Emotiv;
 
 namespace Mind_Control
 {
@@ -20,9 +23,51 @@ namespace Mind_Control
     /// </summary>
     public partial class MainWindow : Window
     {
+        private EmoEngine emoEngine;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Tick += dispatcherTimer_OnTick;
+            dt.Interval = new TimeSpan(0, 0, 1);
+            dt.Start();
+
+            emoEngine = EmoEngine.Instance;
+            emoEngine.EmoStateUpdated += engine_EmoStateUpdated;
+            emoEngine.Connect();
+        }
+
+        private void dispatcherTimer_OnTick(object sender, EventArgs e)
+        {
+            try
+            {
+                IntPtr es = EdkDll.EE_EmoStateCreate();
+                HeadsetOnTextBox.Text = Convert.ToString(EdkDll.ES_GetHeadsetOn(es));
+                //emoEngine.ProcessEvents(1000);
+            }
+            catch(EmoEngineException ex)
+            {
+                MessageBox.Show((ex.Message));
+            }
+        }
+
+        private void engine_EmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            EmoState es = e.emoState;
+
+            int nExcitementScore = es.GetHeadsetOn();
+            HeadsetOnTextBox.Text = Convert.ToString(nExcitementScore);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            emoEngine.Disconnect();
+            emoEngine = null;
         }
     }
 }

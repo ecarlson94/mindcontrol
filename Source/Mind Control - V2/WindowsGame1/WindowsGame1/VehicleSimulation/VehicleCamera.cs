fa92d5acf3ddc1bae1708b3e0da1ce85c3a1bff5
Eric Carlson <mcarlson@student.neumont.edu>
2014-05-15 13:52:07 -0700

    Trying to import a car object.

--- Source/Mind Control - V2/WindowsGame1/WindowsGame1/VehicleSimulation/VehicleCamera.cs
+++ Source/Mind Control - V2/WindowsGame1/WindowsGame1/VehicleSimulation/VehicleCamera.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.PeerToPeer.Collaboration;
 using System.Text;
+using WindowsGame1.Managers;
 using DigitalRune.Game;
 using DigitalRune.Game.Input;
 using DigitalRune.Geometry;
+using DigitalRune.Graphics;
+using DigitalRune.Graphics.SceneGraph;
+using DigitalRune.Mathematics;
+using DigitalRune.Mathematics.Algebra;
 using Microsoft.Practices.ServiceLocation;
 
 namespace WindowsGame1.VehicleSimulation
@@ -28,21 +33,75 @@ namespace WindowsGame1.VehicleSimulation
         //----------------------------------------------------------------------
         #region Properties
 
-
+        public CameraNode CameraNode { get; private set; }
 
         #endregion
 
         //----------------------------------------------------------------------
         #region Creation and Cleanup
 
+        public VehicleCamera(IGeometricObject vehicle, IServiceLocator services)
+        {
+            Name = "VehicleCamera";
 
+            _vehicle = vehicle;
+            _services = services;
+            _inputService = services.GetInstance<IInputService>();
+        }
 
         #endregion
 
         //----------------------------------------------------------------------
         #region Overridden Methods
 
+        protected override void OnLoad()
+        {
+            var graphicsService = _services.GetInstance<IGraphicsService>();
 
+            // Define camera projection.
+            var projection = new PerspectiveProjection();
+            projection.SetFieldOfView(
+                ConstantsF.PiOver4,
+                graphicsService.GraphicsDevice.Viewport.AspectRatio,
+                0.1f,
+                1000.0f);
+
+            //Create a camera node
+            CameraNode = new CameraNode(new Camera(projection));
+        }
+
+        protected override void OnUnload()
+        {
+            CameraNode.Dispose(false);
+            CameraNode = null;
+        }
+
+        protected override void OnUpdate(TimeSpan deltaTime)
+        {
+            if (_inputService.EnableMouseCentering)
+            {
+                //float deltaTimeF = (float) deltaTime.TotalSeconds;
+
+                //compute new yaw and pitch from mouse movement here
+                //TBD implement mouse camera rotation
+            }
+
+            CameraNode.LastPoseWorld = CameraNode.PoseWorld;
+
+            var vehiclePose = _vehicle.Pose;
+
+            //Player Camera
+            //Fixed third person
+            Matrix33F yaw = Matrix33F.CreateRotationY(_yaw);
+            Matrix33F pitch = Matrix33F.CreateRotationX(_pitch);
+            Matrix33F orientation = vehiclePose.Orientation * yaw * pitch;
+            Vector3F forward = orientation * -Vector3F.UnitZ;
+            Vector3F up = Vector3F.UnitY;
+            Vector3F position = vehiclePose.Position - 10 * forward + 5 * up;
+            Vector3F target = vehiclePose.Position + 1 * up;
+
+            CameraNode.View = Matrix44F.CreateLookAt(position, target, up);
+        }
 
         #endregion
     }

1750ad0168a415aed5d34a380f0d7452d5616cbf
Eric Carlson <mcarlson@student.neumont.edu>
2014-05-19 12:55:01 -0600

    3D world completed. Need to implement it in the menu.

--- Source/Mind Control - V2/WindowsGame1/WindowsGame1/VehicleSimulation/VehicleComponent.cs
+++ Source/Mind Control - V2/WindowsGame1/WindowsGame1/VehicleSimulation/VehicleComponent.cs
@@ -1,28 +1,170 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using WindowsGame1.Components;
 using WindowsGame1.Managers;
+using WindowsGame1.Windows;
+using DigitalRune.Game.UI;
+using DigitalRune.Game.UI.Controls;
+using DigitalRune.Game.UI.Rendering;
+using DigitalRune.Graphics;
 using DigitalRune.Physics.ForceEffects;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using WindowsGame1.BackgroundObjects;
+using Microsoft.Xna.Framework.Input;
 
 namespace WindowsGame1.VehicleSimulation
 {
     public class VehicleComponent : GraphicsBaseComponent
     {
-        private readonly CognitivVehicle _cognitivVehicle;
-        private readonly VehicleCamera _vehicleCamera;
+        //--------------------------------------------------------------
+        #region Fields
+
+        private readonly DelegateGraphicsScreen _graphicsScreen;
+        private ContentManager _uiContentManager;
+        private UIScreen _uiScreen;
+
+        private CognitivVehicle _cognitivVehicle;
+        private VehicleCamera _vehicleCamera;
         //scene goes here...
+
+        #endregion
+
+        //--------------------------------------------------------------
+        #region Creation and Cleanup
 
         protected VehicleComponent(Game game, EmoEngineManager emoEngine)
             : base(game, emoEngine)
         {
-            //Add basic force effects
-            Simulation.ForceEffects.Add(new Gravity());
-            Simulation.ForceEffects.Add(new Damping());
+            EnableMouseCentering = false;
 
+            _graphicsScreen = new DelegateGraphicsScreen(GraphicsService)
+            {
+                RenderCallback = Render,
+            };
+            GraphicsService.Screens.Insert(0, _graphicsScreen);
 
+            CreateGUI();
         }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Remove UIScreen from UIService
+                UIService.Screens.Remove(_uiScreen);
+
+                //Dispose the current ContentManager
+                _uiContentManager.Dispose();
+
+                // Remove graphics screen from graphics service
+                GraphicsService.Screens.Remove(_graphicsScreen);
+            }
+            base.Dispose(disposing);
+        }
+
+        #endregion
+
+        //--------------------------------------------------------------
+        #region Private Methods
+
+        private void CreateGUI()
+        {
+            // Dispose old UI.
+            if (_uiContentManager != null)
+            {
+                _uiContentManager.Dispose();
+                UIService.Screens.Remove(_uiScreen);
+            }
+
+            LoadTheme();
+
+            var directionCheck = new DirectionCheckboxWindow(EmoEngine);
+            directionCheck.Closing += DirectionCheckOnClosing;
+            _uiScreen.Children.Add(directionCheck);
+        }
+
+        private void DirectionCheckOnClosing(object sender, CancelEventArgs cancelEventArgs)
+        {
+            var actions = (sender as DirectionCheckboxWindow).AllowedActions;
+            if (actions.Count() > 0)
+            {
+                //Add basic force effects
+                Simulation.ForceEffects.Add(new Gravity());
+                Simulation.ForceEffects.Add(new Damping());
+
+                //Add the sky and ground objects here
+                GameObjectService.Objects.Add(new Sky(Services));
+                GameObjectService.Objects.Add(new Ground(Services));
+
+                //Add the game object which controls a vehicle here
+                _cognitivVehicle = new CognitivVehicle(Services, EmoEngine, actions);
+                GameObjectService.Objects.Add(_cognitivVehicle);
+
+                //Add the camera object attached to chassis of the vehicle
+                _vehicleCamera = new VehicleCamera(_cognitivVehicle.Vehicle.Chassis, Services);
+                GameObjectService.Objects.Add(_vehicleCamera);
+                GraphicsScreen.CameraNode3D = _vehicleCamera.CameraNode;
+
+                DisposeGUI();
+            }
+            else
+                cancelEventArgs.Cancel = true;
+        }
+
+        private void DisposeGUI()
+        {
+            if (_uiContentManager != null)
+            {
+                _uiContentManager.Dispose();
+                UIService.Screens.Remove(_uiScreen);
+            }
+        }
+
+        private void LoadTheme()
+        {
+            // Load a UI theme, which defines the appearance and default values of UI controls.
+            _uiContentManager = new ContentManager(Game.Services, "NeoforceTheme");
+            Theme theme = _uiContentManager.Load<Theme>("ThemeRed");
+            // Create a UI renderer, which uses the theme info to renderer UI controls.
+            UIRenderer renderer = new UIRenderer(Game, theme);
+
+            // Create a UIScreen and add it to the UI service. The screen is the root of 
+            // the tree of UI controls. Each screen can have its own renderer. 
+            _uiScreen = new UIScreen("Default", renderer)
+            {
+                // Make the screen transparent.
+                Background = new Color(0, 0, 0, 0),
+            };
+
+            // Add the screen to the UI service.
+            UIService.Screens.Add(_uiScreen);
+        }
+
+        private void Render(RenderContext context)
+        {
+            if (_uiScreen != null)
+                _uiScreen.Draw(context.DeltaTime);
+        }
+
+        #endregion
+
+        //--------------------------------------------------------------
+        #region Overridden Methods
+
+        public override void Update(GameTime gameTime)
+        {
+            if (InputService.IsPressed(Keys.Escape, true))
+            {
+                Game.Components.Add(new MenuComponent(Game, EmoEngine));
+                Game.Components.Remove(this);
+                Dispose();
+            }
+            base.Update(gameTime);
+        }
+
+        #endregion
     }
 }
---
+++ Source/Mind Control - V2/WindowsGame1/WindowsGame1/Windows/DirectionCheckboxWindow.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using WindowsGame1.Managers;
+using DigitalRune.Game.UI;
+using DigitalRune.Game.UI.Controls;
+using DigitalRune.Mathematics.Algebra;
+using Emotiv;
+
+namespace WindowsGame1.Windows
+{
+    public class DirectionCheckboxWindow : Window
+    {
+        private EmoEngineManager _emoEngine;
+
+        public List<EdkDll.EE_CognitivAction_t> AllowedActions { get; private set; }
+
+        public DirectionCheckboxWindow(EmoEngineManager emoEngine)
+        {
+            _emoEngine = emoEngine;
+
+            AllowedActions = new List<EdkDll.EE_CognitivAction_t>();
+            Name = "DirectionCheckboxWindow";
+            Title = "Choose Directions";
+            CloseButtonStyle = String.Empty;
+
+            CreateGUI();
+        }
+
+        private void CreateGUI()
+        {
+            var stackPanel = new StackPanel()
+            {
+                Name = "DirectionPanel",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Orientation = Orientation.Vertical,
+                Margin = new Vector4F(5),
+            };
+            Content = stackPanel;
+
+            var message = new TextBlock
+            {
+                Text = "Choose directions to practice.",
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Vector4F(4),
+            };
+            stackPanel.Children.Add(message);
+
+            var forwardCheck = new CheckBox
+            {
+                Margin = new Vector4F(2),
+                Content = new TextBlock{ Text = "Forward" },
+                IsEnabled = _emoEngine.IsCognitivActionTrained(EdkDll.EE_CognitivAction_t.COG_PUSH),
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            forwardCheck.Click += (s, e) =>
+            {
+                if (AllowedActions.Contains(EdkDll.EE_CognitivAction_t.COG_PUSH))
+                    AllowedActions.Remove(EdkDll.EE_CognitivAction_t.COG_PUSH);
+                else
+                    AllowedActions.Add(EdkDll.EE_CognitivAction_t.COG_PUSH);
+            };
+            stackPanel.Children.Add(forwardCheck);
+
+            var backwardCheck = new CheckBox
+            {
+                Margin = new Vector4F(2),
+                Content = new TextBlock { Text = "Backward" },
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                IsEnabled = _emoEngine.IsCognitivActionTrained(EdkDll.EE_CognitivAction_t.COG_PULL),
+            };
+            backwardCheck.Click += (s, e) =>
+            {
+                if (AllowedActions.Contains(EdkDll.EE_CognitivAction_t.COG_PULL))
+                    AllowedActions.Remove(EdkDll.EE_CognitivAction_t.COG_PULL);
+                else
+                    AllowedActions.Add(EdkDll.EE_CognitivAction_t.COG_PULL);
+            };
+            stackPanel.Children.Add(backwardCheck);
+
+            var leftCheck = new CheckBox
+            {
+                Margin = new Vector4F(2),
+                Content = new TextBlock { Text = "Turn Left" },
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                IsEnabled = _emoEngine.IsCognitivActionTrained(EdkDll.EE_CognitivAction_t.COG_LEFT),
+            };
+            leftCheck.Click += (s, e) =>
+            {
+                if (AllowedActions.Contains(EdkDll.EE_CognitivAction_t.COG_LEFT))
+                    AllowedActions.Remove(EdkDll.EE_CognitivAction_t.COG_LEFT);
+                else
+                    AllowedActions.Add(EdkDll.EE_CognitivAction_t.COG_LEFT);
+            };
+            stackPanel.Children.Add(leftCheck);
+
+            var rightCheck = new CheckBox
+            {
+                Margin = new Vector4F(2),
+                Content = new TextBlock { Text = "Turn Right" },
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                IsEnabled = _emoEngine.IsCognitivActionTrained(EdkDll.EE_CognitivAction_t.COG_RIGHT),
+            };
+            rightCheck.Click += (s, e) =>
+            {
+                if (AllowedActions.Contains(EdkDll.EE_CognitivAction_t.COG_RIGHT))
+                    AllowedActions.Remove(EdkDll.EE_CognitivAction_t.COG_RIGHT);
+                else
+                    AllowedActions.Add(EdkDll.EE_CognitivAction_t.COG_RIGHT);
+            };
+            stackPanel.Children.Add(rightCheck);
+
+            var okButton = new Button
+            {
+                Margin = new Vector4F(3),
+                Content = new TextBlock{ Text = "Ok" },
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            okButton.Click += (s, e) => Close();
+            stackPanel.Children.Add(okButton);
+        }
+    }
+}
